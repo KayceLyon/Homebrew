cbf0591f3d0725d2a4989a85f3f59161c54e3dd7
Kayce Hubbard <ghuleh@Nameless-Ghuleh.local>
2022-11-25 16:44:38 -0600

    Current user session in Spells get.

--- controllers/sessionsController.js
+++ controllers/sessionsController.js
@@ -10,23 +10,30 @@ sessions.get('/new', (req, res) => {
 });
 
 sessions.post('/new', (req, res) => {
-    User.findOne({username: req.body.username}, (err, foundUser) => {
-        if(err) {
-            console.log(err);
-            res.send('DB does not work.');
-        } else if(!foundUser) {
-            res.send('<a href="/"User not found. </a>');
+    User.findOne({ username: req.body.username }, (err, foundUser) => {
+        // Database error
+        if (err) {
+          console.log(err)
+          res.send('oops the db had a problem')
+        } else if (!foundUser) {
+          // if found user is undefined/null not found etc
+          res.send('<a  href="/">Sorry, no user found </a>')
         } else {
-            if(bcrypt.compareSync(req.body.password, foundUser.password)) {
-                req.session.currentUser = foundUser;
-                res.redirect('/');
-            } else {
-                res.send('<a href="/" Incorrect password. </a>');
-            }
-        };
+          // user is found yay!
+          // now let's check if passwords match
+          if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+            // add the user to our session
+            req.session.currentUser = foundUser
+            // redirect back to our home page
+            res.redirect('/')
+          } else {
+            // passwords do not match
+            res.send('<a href="/"> password does not match </a>')
+          }
+        }
+      });
     });
-
-});
+    
 
 
 sessions.delete('/', (req, res) => {
