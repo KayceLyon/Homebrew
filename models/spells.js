22607bffaeaedb4d97704c4b39d860c976d1043e
Kayce Hubbard <ghuleh@Nameless-Ghuleh.local>
2022-11-29 19:26:49 -0600

    Spell form rework

--- models/spells.js
+++ models/spells.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const spellSchema = new Schema ({
-    name: {type: String},
+    name: {type: String, required: true},
     level: {type: Number},
     school: {type: String},
     ritual: {type: String},
