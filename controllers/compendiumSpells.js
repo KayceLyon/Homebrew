8f0a0cda7b45073bec9fe27f6a4f1930c51884cc
Kayce Hubbard <ghuleh@Nameless-Ghuleh.local>
2022-11-30 08:31:43 -0600

    Spell: New, Show, Edit, Index

---
+++ controllers/compendiumSpells.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const compendiumSpells = express.Router()
+const compendiumSpell = require('../models/compendiumSpells.js')
+
+// Create: New
+compendiumSpells.get('/new', (req, res) => {
+    res.render(
+        'compendiumSpellNew.ejs',
+        {
+            currentUser: req.session.currentUser
+        })
+});
+
+// Create: Post
+compendiumSpells.post('/', (req, res) => {
+    compendiumSpell.create(req.body, (err, data) => {
+        console.log(req.body);
+            if(err){
+           console.log(err)
+            } else {
+            console.log(data)
+           }
+        res.redirect('/compendiumspells');
+    });
+});
+
+// Read: Index
+compendiumSpells.get('/', (req, res) => {
+    compendiumSpell.find({}, (err, foundSpells) => {
+        res.render(
+            'compendiumSpellIndex.ejs', 
+            {
+                spell: foundSpells,
+                currentUser: req.session.currentUser
+            }
+        )
+    });
+});
+
+// Read: Show
+compendiumSpells.get('/:id', (req, res) => {
+    compendiumSpell.findById(req.params.id, (err, spellId) => {
+        res.render(
+            'compendiumSpellShow.ejs',
+            {
+                spell: spellId,
+                currentUser: req.session.currentUser
+            }
+        )
+    });
+});
+
+// Update: Edit
+compendiumSpells.get('/:id/edit', (req, res) => {
+    compendiumSpell.findById(req.params.id, (err, spellId) => {
+        res.render(
+            'compendiumSpellEdit.ejs',
+            {
+                spell: spellId,
+                currentUser: req.session.currentUser
+            }
+        )
+    });
+});
+
+// Update: Put
+compendiumSpells.put('/:id', (req, res) => {
+    compendiumSpell.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updateModel) => {
+        res.redirect('/compendiumspells');
+    });
+});
+
+// Destroy: Delete
+compendiumSpells.delete('/:id', (req, res) => {
+    compendiumSpell.findByIdAndRemove(req.params.id, (err, spell) => {
+        res.redirect('/compendiumspells')
+    })
+});
+
+module.exports = compendiumSpells;
