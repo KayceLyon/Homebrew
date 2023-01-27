b06e06e6e5d1e255eea4e904eddeeccd1b78e582
Kayce Hubbard <ghuleh@Nameless-Ghuleh.local>
2023-01-27 10:25:46 -0600

    updated armor schema

--- models/armor.js
+++ models/armor.js
@@ -3,22 +3,14 @@ const Schema = mongoose.Schema;
 
 const armorSchema = new Schema ({
     name: {type: String, required: true},
-    // level: {type: Number, required: true},
-    // school: {type: String, required: true} ,
-    // ritual: {type: String},
-    // castingTime: {type: Number},
-    // range: {type: Number},
-    // components: {type: String},
-    // materials: {type: String},
-    // duration: {type: Number},
-    // description: {type: String, required: true},
-    // classes: {type: String, required: true} ,
-    // diceRolls: {type: Number},
-    // diceSides: {type: Number},
-    // saveType: {type: String},
-    // attackType: {type: String},
-    // damageType: {type: String},
-    // conditions: {type: String},
+    type: {type: String},
+    weight: {type: Number},
+    cost: {type: Number},
+    currency: {type:String},
+    property: {type: String},
+    stealth: {type: String},
+    armorClass: {type: Number},
+    description: {type: String} 
 })
 
 const Armor = mongoose.model('Armor', armorSchema);
