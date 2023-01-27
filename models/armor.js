7b24c5cccd30b8342bc7412a7f2fda179ea688f9
Kayce Hubbard <ghuleh@Nameless-Ghuleh.local>
2023-01-27 10:29:10 -0600

    Comment out schema

--- models/armor.js
+++ models/armor.js
@@ -3,14 +3,14 @@ const Schema = mongoose.Schema;
 
 const armorSchema = new Schema ({
     name: {type: String, required: true},
-    type: {type: String},
-    weight: {type: Number},
-    cost: {type: Number},
-    currency: {type:String},
-    property: {type: String},
-    stealth: {type: String},
-    armorClass: {type: Number},
-    description: {type: String} 
+    // type: {type: String},
+    // weight: {type: Number},
+    // cost: {type: Number},
+    // currency: {type:String},
+    // property: {type: String},
+    // stealth: {type: String},
+    // armorClass: {type: Number},
+    // description: {type: String} 
 })
 
 const Armor = mongoose.model('Armor', armorSchema);
